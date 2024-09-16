e10112263d1c1d7a220efbe8d83773390f1dd0e5
ckckckcz <rriovld@gmail.com>
2024-09-17 06:29:58 +0700

    feat: section showcase bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -24,6 +24,85 @@ export const Header = () => {
           <Right />
         </div>
       </section>
+      <section className="bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl w-full lg:py-16">
+          <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
+            <li className="me-2">
+              <a href="#" aria-current="page" className="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500">
+                Minggu 1
+              </a>
+            </li>
+            <li className="me-2">
+              <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                Minggu 2
+              </a>
+            </li>
+            <li className="me-2">
+              <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                Minggu 3
+              </a>
+            </li>
+            <li className="me-2">
+              <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                Minggu 4
+              </a>
+            </li>
+            <li className="me-2">
+              <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                Minggu 5
+              </a>
+            </li>
+            <li className="me-2">
+              <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                Minggu 6
+              </a>
+            </li>
+            <li className="me-2">
+              <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                Minggu 7
+              </a>
+            </li>
+            <li className="me-2">
+              <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                Minggu 8
+              </a>
+            </li>
+            <li className="me-2">
+              <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                Minggu 9
+              </a>
+            </li>
+            <li className="me-2">
+              <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                Minggu 10
+              </a>
+            </li>
+            <li className="me-2">
+              <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                Minggu 11
+              </a>
+            </li>
+            <li className="me-2">
+              <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                Minggu 12
+              </a>
+            </li>
+            <li className="me-2">
+              <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                Minggu 13
+              </a>
+            </li>
+            <li className="me-2">
+              <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                Minggu 14
+              </a>
+            </li>
+            <li>
+              <a className="inline-block p-4 text-gray-400 rounded-t-lg cursor-not-allowed dark:text-gray-500">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </section>
     </>
   );
 };
